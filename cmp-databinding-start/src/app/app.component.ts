57b1aae3be14208446de7e1f40823c3a239a3b68
Paul Theby <paultheby@gmail.com>
2023-10-09 19:56:29 -0500

    Few Videos since Committing

--- cmp-databinding-start/src/app/app.component.ts
+++ cmp-databinding-start/src/app/app.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
-export class AppComponent {
-  serverElements = [{type:'server', name: 'Testserver', content: 'Just a test!'}];
-}
+export class AppComponent implements OnInit {
+  serverElements = [
+    { type: "server", name: "Testserver", content: "Just a test!" },
+  ];
 
+  // method
+  ngOnInit(): void {}
+
+  // method
+  onServerAdded(serverData: { serverName: string; serverContent: string }) {
+    this.serverElements.push({
+      type: "server",
+      name: serverData.serverName,
+      content: serverData.serverContent,
+    });
+  }
+
+  // method
+  onBlueprintAdded(blueprintData: {
+    serverName: string;
+    serverContent: string;
+  }) {
+    this.serverElements.push({
+      type: "blueprint",
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent,
+    });
+  }
+}
