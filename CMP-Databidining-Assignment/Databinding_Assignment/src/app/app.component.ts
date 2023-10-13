d799eb07dacabbe0d3a7d627546008301b6b6e10
Paul Theby <paultheby@gmail.com>
2023-10-13 13:40:45 -0500

    Update app.component.ts

--- CMP-Databidining-Assignment/Databinding_Assignment/src/app/app.component.ts
+++ CMP-Databidining-Assignment/Databinding_Assignment/src/app/app.component.ts
@@ -3,13 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  oddNumbers:number[]=[];
-  evenNumbers:number[]=[];
+  oddNumbers: number[] = [];
+  evenNumbers: number[] = [];
 
-  onIntervalFired(firedNumber:number) {
+  onIntervalFired(firedNumber: number) {
     if (firedNumber % 2 === 0) {
       this.evenNumbers.push(firedNumber);
     } else {
